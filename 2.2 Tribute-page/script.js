b5a99370e3d141cb9c0b4f6a81b0f4ad894eca54
Abhishek7487 <dellabhishek7487@gmail.com>
2023-10-22 14:21:29 +0530

    2.2 Adding global styels, variables, favicon

---
+++ 2.2 Tribute-page/script.js
@@ -0,0 +1,28 @@
+/* Google Font CDN */
+@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Montserrat:wght@300;400;500;600&display=swap');
+
+/* Global Styles */
+
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+html{
+  font-size: 62.5%;
+}
+
+/* CSS Variables */
+
+:root{
+  /* Colors */
+  --color-white: #fff;
+  --color-dark: #000;
+  --color-dark-mid: #1a1a1a;
+  --color-dark-grey: #7a7a7a;
+
+  /* Fonts */
+  --font-primary: 'Bebas Neue', sans-serif; 
+  --font-secondary: 'Montserrat', sans-serif; 
+}
