1809e2680ac2914519e165b89ac6849665ace0e8
Abhishek7487 <dellabhishek7487@gmail.com>
2023-10-31 09:19:37 +0530

    2.3 Final: Added customized scrollbar

--- 2.3 Todo-webapp/src/features/todo/TodoList.jsx
+++ 2.3 Todo-webapp/src/features/todo/TodoList.jsx
@@ -1,6 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
 import TodoItem from "./TodoItem";
-import { useState } from "react";
 import { filterTodoList } from "./todoSlice";
 
 function TodoList() {
