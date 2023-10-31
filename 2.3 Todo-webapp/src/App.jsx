82e7b3479e952098b9a2ad4b1a28b03a327f2e27
Abhishek7487 <dellabhishek7487@gmail.com>
2023-10-31 18:47:48 +0530

    2.3 Added few blobs to the web app

--- 2.3 Todo-webapp/src/App.jsx
+++ 2.3 Todo-webapp/src/App.jsx
@@ -3,6 +3,9 @@ import TodoCard from "./features/todo/TodoCard";
 function App() {
   return (
     <div className="app">
+      <div className="blob1"></div>
+      <div className="blob2"></div>
+      <div className="blob3"></div>
       <TodoCard />
     </div>
   );
